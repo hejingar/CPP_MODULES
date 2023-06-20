ed7cf17e141496bbe72f61bc05cc1aac489d4caa
ael-youb <hejingoh@gmail.com>
2023-06-20 18:07:16 +0200

    cpp01

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-youb <ael-youb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 22:15:23 by ael-youb          #+#    #+#             */
-/*   Updated: 2023/04/30 22:23:09 by ael-youb         ###   ########.fr       */
+/*   Updated: 2023/06/19 16:55:01 by ael-youb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,14 @@
 int main(void)
 {
 	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF =  str;
+
+	std::cout << "String variable address: " << &str << std::endl;
+	std::cout << "String pointer held : " << stringPTR << std::endl;
+	std::cout << "String ref held : " << &stringREF << std::endl;
+
+	std::cout << "String value : " << str << std::endl;
+	std::cout << "String value : " << *stringPTR << std::endl;
+	std::cout << "String value : " << stringREF << std::endl;
 }
