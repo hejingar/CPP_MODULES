cb36a4018df10f8cedb1bdc353f03ca73dd82c68
ael-youb <ael-youb@student.42.fr>
2023-07-03 00:39:55 +0200

    string replacer in file good

--- CPP01/ex04/Replacer.cpp
+++ CPP01/ex04/Replacer.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-youb <ael-youb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 21:00:50 by ael-youb          #+#    #+#             */
-/*   Updated: 2023/07/02 22:38:18 by ael-youb         ###   ########.fr       */
+/*   Updated: 2023/07/03 00:38:37 by ael-youb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 Replacer::Replacer(std::string filename, std::string s1, std::string s2)
 {
+	std::string replace = filename + ".replace";
 	this->_s1 = s1;
 	this->_s2 = s2;
-	_origin_file.open(filename);
-	_replaced_file.open(filename + ".replace");
+	_origin_file.open(filename.c_str());
+	_replaced_file.open(replace.c_str());
 }
 
 Replacer::~Replacer()
@@ -35,15 +36,18 @@ void	Replacer::readAndReplace(void)
 	{
 		while(getline(this->_origin_file, buffer))
 		{
-			while(pos += this->_s2.length())
+			while(pos < buffer.length())
 			{
 				pos = buffer.find(this->_s1, pos);
 				if (pos == std::string::npos)
             		break;
 				buffer.erase(pos, this->_s1.length());
         		buffer.insert(pos, this->_s2);
+				pos += this->_s2.length();
+				//fun fact : incrementer de 1 et mettre s1 = toto et s2 = ttoto fait une boucle infinie
 			}
-			this->_replaced_file << buffer;
+			this->_replaced_file << buffer + "\n";
+			pos = 0;
 		}
 	}
 	else
--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,21 @@
 /*   By: ael-youb <ael-youb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 20:51:37 by ael-youb          #+#    #+#             */
-/*   Updated: 2023/07/02 20:51:38 by ael-youb         ###   ########.fr       */
+/*   Updated: 2023/07/03 00:19:01 by ael-youb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Replacer.hpp"
+
+int main(int ac, char **av)
+{
+	if (ac < 4)
+		std::cout << "Not enough arguments, quitting..." << std::endl;
+	else
+	{
+		Replacer* replacer = new Replacer(av[1], av[2], av[3]);
+		replacer->readAndReplace();
+		delete replacer;
+	}
+	return (0);
+}
