5759ec74829a037cbf2f11dd60da26dd07144e7a
ael-youb <ael-youb@student.42.fr>
2023-07-03 02:03:30 +0200

    Harl tests

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-youb <ael-youb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 01:18:05 by ael-youb          #+#    #+#             */
-/*   Updated: 2023/07/03 01:19:56 by ael-youb         ###   ########.fr       */
+/*   Updated: 2023/07/03 02:03:05 by ael-youb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,16 @@ int	main( void )
 	Harl harl = Harl();
 	
 	harl.complain("debug");
+	std::cout << std::endl;
 	harl.complain("warning");
+	std::cout << std::endl;
 	harl.complain("ez");
+	std::cout << std::endl;
 	harl.complain("error");
+	std::cout << std::endl;
 	harl.complain("info");
+	std::cout << std::endl;
 	harl.complain("no");
+	std::cout << std::endl;
 	return (0);	
 }
